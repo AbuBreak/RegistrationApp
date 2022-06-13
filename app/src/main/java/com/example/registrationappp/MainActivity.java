a51a0016562b041cc10bb806196bd96323d011c7
AbuBreak <94977176+AbuBreak@users.noreply.github.com>
2022-06-13 22:03:54 +0300

    Third commit

--- app/src/main/java/com/example/registrationappp/MainActivity.java
+++ app/src/main/java/com/example/registrationappp/MainActivity.java
@@ -38,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
         initViews();
 
         System.out.println("First Change");
-        Today is a good day!
+        System.out.println("Problem Solved!");
         btnPickImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
